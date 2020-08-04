c40e79c12266df712aa150b5341c18bafd61d00f
zhangwei95 <574150903@qq.com>
2020-08-04 09:36:31 +0800

    Hystrix sample

---
+++ hystrix-sample/src/main/java/com/zhangwei95/hystrixsample/HystrixSampleApplication.java
@@ -0,0 +1,13 @@
+package com.zhangwei95.hystrixsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixSampleApplication.class, args);
+    }
+
+}
