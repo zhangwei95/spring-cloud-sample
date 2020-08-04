d0da9baf7667dcb4fe5f312b8eaafb76ff48a5f5
zhangwei95 <574150903@qq.com>
2020-08-04 10:15:07 +0800

    public api

--- eureka-service-provider/src/main/java/com/zhangwei95/controller/TestController.java
+++ eureka-service-provider/src/main/java/com/zhangwei95/controller/TestController.java
@@ -1,5 +1,8 @@
 package com.zhangwei95.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
  * @Describe 类描述
  * @date 2020/6/22
  */
+@Api(tags = "test")
 @RestController
 @RequestMapping(value = "/api")
 public class TestController {
 
     @GetMapping(value = "/test")
-    public String getMessage(){
+    @ApiOperation(value = "testApi", httpMethod = "GET", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public String getMessage() throws InterruptedException {
+//        Thread.sleep(1000);
+        return "hello";
+    }
+
+    @GetMapping(value = "/public/test")
+    @ApiOperation(value = "unAuthApi", httpMethod = "GET", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public String getMessage1() throws InterruptedException {
+//        Thread.sleep(1000);
         return "hello";
     }
 }
